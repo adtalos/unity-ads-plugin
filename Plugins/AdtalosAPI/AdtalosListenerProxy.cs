9436dff76b778ef77722e60ba39a623043b8ba7c
andot <mabingyao@gmail.com>
2019-06-18 18:25:02 +0800

    增加插件代码

---
+++ Plugins/AdtalosAPI/AdtalosListenerProxy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+namespace Adtalos {
+    public delegate void AdtalosListener(string adUnitId, string name, string data);
+
+#if UNITY_ANDROID
+    public class AdtalosListenerProxy : AndroidJavaProxy {
+        private AdtalosListener listener;
+        internal AdtalosListenerProxy(AdtalosListener listener)
+            : base("com.adtalos.ads.plugin.IAdtalosListener") {
+            this.listener = listener;
+        }
+        void onAdtalosEvent(string adUnitId, string name, string data) {
+            Debug.Log("c# adtalos listener proxy " + adUnitId + " " + name + " " + data);
+            listener?.Invoke(adUnitId, name, data);
+        }
+        override public string toString() {
+            return "AdtalosListenerProxy";
+        }
+    }
+#endif
+}
---
+++ Plugins/AdtalosAPI/AdtalosVideoMetadata.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+namespace Adtalos {
+    [Serializable]
+    public class AdtalosVideoMetadata {
+        public double currentTime;
+        public double duration;
+        public double videoWidth;
+        public double videoHeight;
+        public bool autoplay;
+        public bool muted;
+        public double volume;
+        public AdtalosVideoMetadata(string json) {
+            JsonUtility.FromJsonOverwrite(json, this);
+        }
+        public override string ToString() {
+            return JsonUtility.ToJson(this);
+        }
+    }
+}
