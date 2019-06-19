0517ec4a481426e3d58dc16ee11578e6f9b3a205
andot <mabingyao@gmail.com>
2019-06-19 22:44:34 +0800

    Unity 插件的演示实例

---
+++ Test/Assets/Plugins/AdmobAPI/AdtalosUnityPlugin.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using UnityEngine.Android;
+namespace Adtalos {
+    public class AdtalosUnityPlugin {
+        private static AdtalosUnityPlugin _instance;
+
+        public static AdtalosUnityPlugin Instance() {
+            if (_instance == null) {
+                _instance = new AdtalosUnityPlugin();
+                _instance.PreInitAdtalos();
+            }
+            return _instance;
+        }
+#if UNITY_ANDROID
+        private AndroidJavaObject jadtalos;
+
+        private void PreInitAdtalos() {
+            if (jadtalos == null) {
+                AndroidJavaClass adtalosUnityPluginClass = new AndroidJavaClass("com.adtalos.ads.plugin.AdtalosUnityPlugin");
+                jadtalos = adtalosUnityPluginClass.CallStatic<AndroidJavaObject>("getInstance");
+                if (!Permission.HasUserAuthorizedPermission("android.permission.READ_PHONE_STATE")) {
+                    Permission.RequestUserPermission("android.permission.READ_PHONE_STATE");
+                }
+            }
+        }
+        public void ShowBannerAbsolute(string adUnitId, int width, int height, int x, int y = 0, AdtalosListener listener = null) {
+            jadtalos.Call("showBannerAbsolute", new object[] { adUnitId, width, height, x, y, new AdtalosListenerProxy(listener) });
+        }
+        public void ShowBannerRelative(string adUnitId, int width, int height, int position, int y = 0, AdtalosListener listener = null) {
+            jadtalos.Call("showBannerRelative", new object[] { adUnitId, width, height, position, y, new AdtalosListenerProxy(listener) });
+        }
+        public void ShowNativeAbsolute(string adUnitId, int width, int height, int x, int y = 0, AdtalosListener listener = null) {
+            jadtalos.Call("showNativeAbsolute", new object[] { adUnitId, width, height, x, y, new AdtalosListenerProxy(listener) });
+        }
+        public void ShowNativeRelative(string adUnitId, int width, int height, int position, int y = 0, AdtalosListener listener = null) {
+            jadtalos.Call("showNativeRelative", new object[] { adUnitId, width, height, position, y, new AdtalosListenerProxy(listener) });
+        }
+        public void LoadInterstitialAd(string adUnitId, bool immersiveMode = true, AdtalosListener listener = null) {
+            jadtalos.Call("loadInterstitialAd", new object[] { adUnitId, immersiveMode, new AdtalosListenerProxy(listener) });
+        }
+        public void LoadSplashAd(string adUnitId, AdtalosListener listener = null) {
+            jadtalos.Call("loadSplashAd", new object[] { adUnitId, new AdtalosListenerProxy(listener) });
+        }
+        public void LoadRewardedVideoAd(string adUnitId, AdtalosListener listener = null) {
+            jadtalos.Call("loadRewardedVideoAd", new object[] { adUnitId, new AdtalosListenerProxy(listener) });
+        }
+        public void IsLoaded(string adUnitId) {
+            jadtalos.Call("isLoaded", new object[] { adUnitId });
+        }
+        public void Show(string adUnitId) {
+            jadtalos.Call("show", new object[] { adUnitId });
+        }
+        public void PlayVideo(string adUnitId) {
+            jadtalos.Call("playVideo", new object[] { adUnitId });
+        }
+        public void PauseVideo(string adUnitId) {
+            jadtalos.Call("pauseVideo", new object[] { adUnitId });
+        }
+        public void MuteVideo(string adUnitId, bool mute) {
+            jadtalos.Call("muteVideo", new object[] { adUnitId, mute });
+        }
+        public bool HasVideo(string adUnitId) {
+            return jadtalos.Call<bool>("hasVideo", new object[] { adUnitId });
+        }
+        public AdtalosVideoMetadata GetVideoMetaData(string adUnitId) {
+            string data = jadtalos.Call<string>("getVideoMetaData", new object[] { adUnitId });
+            if (data == null) return null;
+            return new AdtalosVideoMetadata(data);
+        }
+        public void Destroy(string adUnitId) {
+            jadtalos.Call("destroy", new object[] { adUnitId });
+        }
+        public void Pause(string adUnitId) {
+            jadtalos.Call("pause", new object[] { adUnitId });
+        }
+        public void Resume(string adUnitId) {
+            jadtalos.Call("resume", new object[] { adUnitId });
+        }
+#else
+        private void PreInitAdtalos() {}
+
+        public void ShowBannerAbsolute(string adUnitId, int width, int height, int x, int y = 0, AdtalosListener listener = null) {
+            Debug.Log("calling ShowBannerAbsolute");
+        }
+        public void ShowBannerRelative(string adUnitId, int width, int height, int x, int y = 0, AdtalosListener listener = null) {
+            Debug.Log("calling ShowBannerRelative");
+        }
+        public void ShowNativeAbsolute(string adUnitId, int width, int height, int x, int y = 0, AdtalosListener listener = null) {
+            Debug.Log("calling ShowNativeAbsolute");
+        }
+        public void ShowNativeRelative(string adUnitId, int width, int height, int position, int y = 0, AdtalosListener listener = null) {
+            Debug.Log("calling ShowNativeRelative");
+        }
+        public void LoadInterstitialAd(string adUnitId, bool immersiveMode = true, AdtalosListener listener = null) {
+            Debug.Log("calling LoadInterstitialAd");
+        }
+        public void LoadSplashAd(string adUnitId, AdtalosListener listener = null) {
+            Debug.Log("calling LoadSplashAd");
+        }
+        public void LoadRewardedVideoAd(string adUnitId, AdtalosListener listener = null) {
+            Debug.Log("calling LoadRewardedVideoAd");
+        }
+        public void IsLoaded(string adUnitId) {
+            Debug.Log("calling IsLoaded");
+        }
+        public void Show(string adUnitId) {
+            Debug.Log("calling Show");
+        }
+        public void PlayVideo(string adUnitId) {
+            Debug.Log("calling PlayVideo");
+        }
+        public void PauseVideo(string adUnitId) {
+            Debug.Log("calling PauseVideo");
+        }
+        public void MuteVideo(string adUnitId, bool mute) {
+            Debug.Log("calling MuteVideo");
+        }
+        public bool HasVideo(string adUnitId) {
+            Debug.Log("calling HasVideo");
+        }
+        public AdtalosVideoMetadata GetVideoMetaData(string adUnitId) {
+            Debug.Log("calling GetVideoMetaData");
+        }
+        public void Destroy(string adUnitId) {
+            Debug.Log("calling destroy");
+        }
+        public void Pause(string adUnitId) {
+            Debug.Log("calling Pause");
+        }
+        public void Resume(string adUnitId) {
+            Debug.Log("calling Resume");
+        }
+
+#endif
+    }
+}
---
+++ Test/Assets/Scenes/AdtalosDemo.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using Adtalos;
+
+public class AdtalosDemo : MonoBehaviour
+{
+    AdtalosUnityPlugin ad;
+    AdtalosListener listener = (adUnitId, name, data) => Debug.Log("adUnitId: " + adUnitId + " event: " + name + " data: " + data);
+    // Start is called before the first frame update
+    void Start()
+    {
+        ad = AdtalosUnityPlugin.Instance();
+        ad.LoadSplashAd("5C3DD65A809B08A2D6CF3DEFBC7E09C7", listener);
+        ad.LoadInterstitialAd("2EF810225D10260506CBB704C96C5325", true, listener);
+        ad.LoadRewardedVideoAd("527E187C5DEA600C35309759469ADAA8", listener);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(100, 100, 320, 100), "show banner ad"))
+        {
+            ad.ShowBannerRelative("209A03F87BA3B4EB82BEC9E5F8B41383", 320, 50, AdPosition.BOTTOM_CENTER, 0, listener);
+        }
+        if (GUI.Button(new Rect(500, 100, 320, 100), "show native ad"))
+        {
+            ad.ShowNativeRelative("98738D91D3BB241458D3FAE5A5BF7D34", -1, -2, AdPosition.MIDDLE_CENTER, 0, listener);
+        }
+        if (GUI.Button(new Rect(100, 300, 320, 100), "show interstitial ad"))
+        {
+            ad.Show("2EF810225D10260506CBB704C96C5325");
+        }
+        if (GUI.Button(new Rect(500, 300, 320, 100), "show rewarded video ad"))
+        {
+            ad.Show("527E187C5DEA600C35309759469ADAA8");
+        }
+    }
+
+    void OnDestroy() {
+        ad.Destroy("209A03F87BA3B4EB82BEC9E5F8B41383");
+        ad.Destroy("98738D91D3BB241458D3FAE5A5BF7D34");
+    }
+
+}
