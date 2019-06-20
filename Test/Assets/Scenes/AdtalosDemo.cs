825b4a199e65ecdc5083b280541c1668d4cda905
andot <mabingyao@gmail.com>
2019-06-20 11:13:59 +0800

    Update AdtalosDemo.cs

--- Test/Assets/Scenes/AdtalosDemo.cs
+++ Test/Assets/Scenes/AdtalosDemo.cs
@@ -1,41 +1,39 @@
 ﻿using UnityEngine;
 using Adtalos;
 
-public class AdtalosDemo : MonoBehaviour
-{
+public class AdtalosDemo : MonoBehaviour {
     AdtalosUnityPlugin ad;
     AdtalosListener listener = (adUnitId, name, data) => Debug.Log("adUnitId: " + adUnitId + " event: " + name + " data: " + data);
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         ad = AdtalosUnityPlugin.Instance();
         ad.LoadSplashAd("5C3DD65A809B08A2D6CF3DEFBC7E09C7", listener);
         ad.LoadInterstitialAd("2EF810225D10260506CBB704C96C5325", true, listener);
         ad.LoadRewardedVideoAd("527E187C5DEA600C35309759469ADAA8", listener);
+        // 显示开屏
+        ad.Show("5C3DD65A809B08A2D6CF3DEFBC7E09C7");
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
 
     }
 
-    private void OnGUI()
-    {
-        if (GUI.Button(new Rect(100, 100, 320, 100), "show banner ad"))
-        {
+    private void OnGUI() {
+        if (GUI.Button(new Rect(100, 100, 320, 100), "show banner ad")) {
+            // 显示横幅
             ad.ShowBannerRelative("209A03F87BA3B4EB82BEC9E5F8B41383", 320, 50, AdPosition.BOTTOM_CENTER, 0, listener);
         }
-        if (GUI.Button(new Rect(500, 100, 320, 100), "show native ad"))
-        {
+        if (GUI.Button(new Rect(500, 100, 320, 100), "show native ad")) {
+            // 显示原生
             ad.ShowNativeRelative("98738D91D3BB241458D3FAE5A5BF7D34", -1, -2, AdPosition.MIDDLE_CENTER, 0, listener);
         }
-        if (GUI.Button(new Rect(100, 300, 320, 100), "show interstitial ad"))
-        {
+        if (GUI.Button(new Rect(100, 300, 320, 100), "show interstitial ad")) {
+            // 显示插屏
             ad.Show("2EF810225D10260506CBB704C96C5325");
         }
-        if (GUI.Button(new Rect(500, 300, 320, 100), "show rewarded video ad"))
-        {
+        if (GUI.Button(new Rect(500, 300, 320, 100), "show rewarded video ad")) {
+            // 显示激励视频
             ad.Show("527E187C5DEA600C35309759469ADAA8");
         }
     }
